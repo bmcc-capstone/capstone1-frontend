7bea86f1e545aef0b1599da25d30a99e3b8966cd
Guarotav <guarotav@gmail.com>
2025-07-17 14:57:45 -0400

    New PollForm Component adadded to components

---
+++ src/components/PollForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const PollForm = () => {
+  const [optionSort, setOptionSort] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const handleOptionClick = () => {};
+
+  const handleSubmit = () => {};
+
+  return <h1></h1>;
+};
