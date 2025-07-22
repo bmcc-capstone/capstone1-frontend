356a3d1d781e13e46e04730c02955239251fa588
jeramyleon <jeramyleon@gmail.com>
2025-07-22 14:35:26 -0400

    share page styling is complete, next step, geting the url to connect to specific pollform

--- src/components/shareableLink.jsx
+++ src/components/shareableLink.jsx
@@ -2,22 +2,16 @@ import React from "react";
 import "./shareableLink.css"
 import { Link } from "react-router-dom";
 
-const ShareableLinkPage = ({ title, description, id}) => {
+const ShareableLinkPage = () => {
     return (
-        <Link to={`/Pollform/${id}`} className="pollCardLink">
-            <div className="pollContainer">
+      <div className="shareableLinkContainer">
+        <h3>✅ Your Poll Has Been Created!</h3>
 
-            <div className="info">
-
-                <h3 id="title">{title}</h3>       
-
-                <p id="description">{description}</p>
-
-            </div>
-
+        <div className="linkInformation">
+          <p>Share this link to collect votes:</p>
+          <textarea>🔗the-link-goes-here</textarea>
         </div>
-        </Link>
-        
+      </div>    
     );
 };
 
