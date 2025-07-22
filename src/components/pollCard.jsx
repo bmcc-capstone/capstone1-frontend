87e41b788eb35594cb108cc9749955258c99e301
Benjamin Ayala <Benjaminayala607@gmail.com>
2025-07-22 15:07:23 -0400

    set feature where if poll is expired does not appear in live polls

--- src/components/pollCard.jsx
+++ src/components/pollCard.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./pollCard.css";
-import { Link } from "react-router-dom";
 
 const PollCard = ({ title, description, id, expires_date, totalVotes }) => {
     return (
-        <Link to={`/Pollform/${id}`} className="pollCardLink">
-            <div className="pollContainer">
-                <div className="info">
-                    <h3 id="title">{title}</h3>
-                    <p id="description">{description}</p>
-                    {expires_date && (
-                        <p className="expires">
-                            Expires: {new Date(expires_date).toLocaleString()}
-                        </p>
-                    )}
-                    <p className="votes">Total Votes: {totalVotes ?? 0}</p>
-                </div>
+        <div className="pollContainer">
+            <div className="info">
+                <h3 id="title">{title}</h3>
+                <p id="description">{description}</p>
+                {expires_date && (
+                    <p className="expires">
+                        Expires: {new Date(expires_date).toLocaleString()}
+                    </p>
+                )}
+                <p className="votes">Total Votes: {totalVotes ?? 0}</p>
             </div>
-        </Link>
+        </div>
     );
 };
-//accepts and displays expiration and vote count
+
 export default PollCard;
