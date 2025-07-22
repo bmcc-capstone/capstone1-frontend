878e0688f2ab26dd0d3a8371f77131b9f20f856f
JoseG2704 <joseg50274@gmail.com>
2025-07-22 12:03:17 -0400

    Fixed LivePolls, connected it and properly working. Currently working on the Vote Page itself and the voting process

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,9 @@ import { API_URL } from "./shared";
 import CreatePollForm from "./components/CreatePollForm";
 import MyPolls from "./components/MyPolls";
 import LivePolls from "./components/LivePolls";
+import LivePoll from "./components/LivePoll";
 import PollForm from "./components/PollForm";
+import GoToVote from "./components/GoToVote";
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -65,8 +67,10 @@ const App = () => {
       <div className="app">
         <Routes>
         <Route path="/CreatePollForm" element={<CreatePollForm user={user} />} />
-        <Route path="/livepoll/:pollId" element={<LivePolls />} />
-          <Route path="/My Polls" element={<MyPolls />} />
+        <Route path="/GoToVote" element={<GoToVote user={user} />} />
+        <Route path="/LivePolls/:pollId" element={<LivePoll />} />
+        <Route path="/LivePolls" element={<LivePolls />} />
+        <Route path="/My Polls" element={<MyPolls />} />
 
           <Route path="/login" element={<Login setUser={setUser} />} />
           <Route path="/signup" element={<Signup setUser={setUser} />} />
